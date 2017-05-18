d866dc3b310f3202afa45f3431ee17242f2c2a9f
Neo <mingtseng.chang@huafu.com.tw>
2017-05-18 14:46:47 +0800

    refactoring ClientActivity

--- client/src/main/java/com/demo/client/ClientActivity.java
+++ client/src/main/java/com/demo/client/ClientActivity.java
@@ -6,7 +6,6 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -15,98 +14,70 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.InetAddress;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ClientActivity extends Activity {
-    public static Handler mHandler = new Handler();
-    Button calcbutton_ECG;
-    private Button transmit;
-    private TextView txtb_ID;
-    private TextView txtb_PSWD;
+    private static Handler mHandler = new Handler();
     private Socket clientSocket = null;
-    private String s = "";
-    private EditText edittb_server;
-    private EditText edittb_ECG_port;
-    private EditText edittb_message;
-    private EditText edittb_accept;
-    String tmp;
-    ServerSocket serverSocket;
-    int serverPort;
+    private EditText mAccept;
+    private String mTranTmp;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        Thread t = new Thread(readData);
-        t.start();
-        findViews();
-        setListeners();
-    }
-
-    private void findViews() {
-        calcbutton_ECG = (Button) findViewById(R.id.submit);
-        transmit = (Button) findViewById(R.id.transmit);
-        edittb_server = (EditText) findViewById(R.id.server);
-        edittb_ECG_port = (EditText) findViewById(R.id.ECG_port);
-        edittb_message = (EditText) findViewById(R.id.message);
-        edittb_message = (EditText) findViewById(R.id.message);
-        edittb_accept = (EditText) findViewById(R.id.ECG_accept);
-    }
-
-    private void setListeners() {
-        calcbutton_ECG.setOnClickListener(calcECG);
-        transmit.setOnClickListener(SendMessage);
-    }
-
-    private Button.OnClickListener calcECG = new Button.OnClickListener() {
-        public void onClick(View v) {
-            InetAddress serverIp;
-            try {
-                serverIp = InetAddress.getByName(edittb_server.getText().toString());
-                int serverPort = Integer.valueOf(edittb_ECG_port.getText().toString());
-                clientSocket = new Socket(serverIp, serverPort); // Connect Server
-                if (clientSocket.isConnected()) {
-                    calcbutton_ECG.setEnabled(false);
-                    s = "USER NAME=";
-                    s += edittb_server.getText().toString() + " ";
-                    s += "SERVER_PORT=";
-                    s += edittb_ECG_port.getText().toString();
-                    Toast.makeText(ClientActivity.this, s, Toast.LENGTH_SHORT).show();
-                    SendData();
-                    try {
-                        Thread.currentThread().sleep(10);//sleep for 1000 ms
-                    } catch (InterruptedException ie) {
+        new Thread(readData).start();
+        final EditText serverip = (EditText) findViewById(R.id.server);
+        final EditText port = (EditText) findViewById(R.id.ECG_port);
+        final EditText input_message = (EditText) findViewById(R.id.message);
+        mAccept = (EditText) findViewById(R.id.ECG_accept);
+        final Button submit = (Button) findViewById(R.id.submit);
+        submit.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InetAddress serverIp;
+                try {
+                    serverIp = InetAddress.getByName(serverip.getText().toString());
+                    int serverPort = Integer.valueOf(port.getText().toString());
+                    clientSocket = new Socket(serverIp, serverPort); // Connect Server
+                    if (clientSocket.isConnected()) {
+                        submit.setEnabled(false);
+                        String s = "USER NAME=";
+                        s += serverip.getText().toString() + " ";
+                        s += "SERVER_PORT=";
+                        s += port.getText().toString();
+                        Toast.makeText(ClientActivity.this, s, Toast.LENGTH_SHORT).show();
+                        SendData(s);
+                        try {
+                            Thread.currentThread().sleep(10);//sleep for 1000 ms
+                        } catch (InterruptedException ie) {
+                        }
                     }
+                } catch (IOException e) {
+
                 }
-            } catch (IOException e) {
-
             }
-
-        }
-    };
-
-    private void SendData() {
-        try {
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-            bw.write(s + "\n");
-            bw.flush();
-        } catch (Exception ex) {
-
-        }
+        });
+        final Button transmit = (Button) findViewById(R.id.transmit);
+        transmit.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SendData(input_message.getText().toString());
+            }
+        });
     }
 
-    private Button.OnClickListener SendMessage = new Button.OnClickListener() {
-        public void onClick(View v) {
+    private void SendData(String message) {
+        if (clientSocket != null) {
             try {
                 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-                bw.write(edittb_message.getText().toString() + "\n");
+                bw.write(message + "\n");
                 bw.flush();
-            } catch (IOException e) {
+            } catch (Exception ex) {
 
             }
         }
-    };
+    }
 
     private Runnable readData = new Runnable() {
         public void run() {
@@ -115,8 +86,8 @@ public class ClientActivity extends Activity {
                     if (clientSocket != null) {
                         while (clientSocket.isConnected()) {
                             BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                            tmp = br.readLine();
-                            if (tmp != null)
+                            mTranTmp = br.readLine();
+                            if (mTranTmp != null)
                                 mHandler.post(updateText);
                         }
                     }
@@ -129,7 +100,7 @@ public class ClientActivity extends Activity {
 
     private Runnable updateText = new Runnable() {
         public void run() {
-            edittb_accept.setText(tmp);
+            mAccept.setText(mTranTmp);
         }
     };
 
