a8092a6b7a4d4ff88deb4625e1ffaecb1632a8e8
Neo <mingtseng.chang@huafu.com.tw>
2017-05-18 12:30:50 +0800

    add config class  for status code

---
+++ server/src/main/java/com/jouhu/tcpserver/C.java
@@ -0,0 +1,10 @@
+package com.jouhu.tcpserver;
+
+/**
+ * Created by Neo on 2017/5/18.
+ */
+
+public class C {
+    public static final int MSG_SERVER_START_ERROR = -1;
+    public static final int RECEIVEDATA = 2;
+}
